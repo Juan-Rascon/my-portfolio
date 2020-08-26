9d1472f694c18243e791a9c5a6b163c72c6dc2b8
jcr <jcrascon+github@gmail.com>
2020-08-25 23:34:02 -0700

    push code

---
+++ src/resumeData.js
@@ -0,0 +1,108 @@
+let resumeData = {
+    "imagebaseurl":"",
+    "name": "Juan Rascon",
+    "role": "Frontend Developer, Product Manager, and Data Scientist",
+    "linkedinId":"linkedIn.com/juanrascon",
+    "skypeid": "Your skypeid",
+    "roleDescription": "",
+    "socialLinks":[
+        {
+          "name":"linkedin",
+          "url":"https://www.linkedin.com/in/juanrascon/",
+          "className":"fa fa-linkedin"
+        },
+        {
+          "name":"github",
+          "url":"https://github.com/jayskratch",
+          "className":"fa fa-github"
+        },
+      ],
+    "aboutme":"My name is Juan Rascon. I majored in Economics from UCLA and while I initially pursued a career in finance, I eventually decided to dive into coding. I enjoy problem-solving and doing research to find solutions to hard problems. I'm currently growing my skill-set to become a more experienced and knowledgable web developer. I may not have the efficiency of a more seasoned developer, but rest assured that I will be 100% dedicated to help you solve your problem.",
+    "address":"USA",
+    "website":"jcrascon@gmail.com",
+    "education":[
+      {
+        "UniversityName":"UCLA",
+        "specialization":"Economics",
+        "MonthOfPassing":"Mar",
+        "YearOfPassing":"2009",
+        "Achievements":"Bachelor's Degree"
+      },
+      {
+        "UniversityName":"UCLA Extension",
+        "specialization":"Certificate In Accounting",
+        "MonthOfPassing":"Jun",
+        "YearOfPassing":"2013",
+        "Achievements":"Some Achievements"
+      },
+      {
+        "UniversityName":"UCLA Extension",
+        "specialization":"Full Stack Web Developer",
+        "MonthOfPassing":"Sep",
+        "YearOfPassing":"2020",
+        "Achievements":"Some Achievements"
+      }
+    ],
+    "work":[
+      {
+        "CompanyName":"Comscore",
+        "specialization":"Product Manager",
+        "MonthOfLeaving":"",
+        "YearOfLeaving":"Present",
+        "Achievements":""
+      },
+      {
+        "CompanyName":"Pulsiam",
+        "specialization":"Operations Manager",
+        "MonthOfLeaving":"Nov",
+        "YearOfLeaving":"2016",
+        "Achievements":""
+      }
+    ],
+    "skillsDescription":"Experience",
+    "skills":[
+      {
+        "skillname":"HTML5"
+      },
+      {
+        "skillname":"CSS"
+      },
+      {
+        "skillname":"Reactjs"
+      }
+    ],
+    "portfolio":[
+      {
+        "name":"One Pager App",
+        "description":"web application",
+        "imgurl":"images/portfolio/tuition.png"
+      },
+      {
+        "name":"IRX",
+        "description":"web app",
+        "imgurl":"images/insightrx_logo_white-teal_920x207.png"
+      },
+      {
+        "name":"project3",
+        "description":"mobileapp",  
+        "imgurl":"images/project_placeholder-3.jpg"
+      },
+      {
+        "name":"project4",
+        "description":"mobileapp",
+        "imgurl":"images/project_placeholder-3.jpg"
+      }
+    ],
+    "testimonials":[
+      {
+        "description":"This is a sample testimonial",
+        "name":"Some technical guy"
+      },
+      {
+        "description":"This is a sample testimonial",
+        "name":"Some technical guy"
+      }
+    ]
+  }
+  
+  export default resumeData
